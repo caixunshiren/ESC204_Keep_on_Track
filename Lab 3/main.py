a346b384e22531c6969bccc6a5df89503f1b1480
David Surry <djsurry@gmail.com>
2022-03-21 16:40:56 -0400

    more cleaning up files

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -9,7 +9,10 @@ server_thread = Thread(target=server.start_server, daemon=True)
 server_thread.start()
 
 # start ui
-ui2.run_ui()
+try:
+	ui2.run_ui()
+except:
+	os.system('rm heat.txt light.txt')
 
 
 
